26b4437048e9621e5bddd40522fffcaf4de092c4
morningstar0000 <miraclemorningstar9@gmail.com>
2024-07-31 12:08:10 +0100

    init

--- 02-portfolio/vite.config.js
+++ 02-portfolio/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base:'/personal-porfolio/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-});
-
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base:'/personal-porfolio/',
-//   plugins: [react()],
-// })
+})
